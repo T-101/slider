41ecf7aa2514105520687ae0b942b2fea9daa691
T-101 <antti.rummukainen@gmail.com>
2023-12-17 20:34:25 +0200

    reveal url field

--- backend/slider/api/v1/serializers.py
+++ backend/slider/api/v1/serializers.py
@@ -1,9 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from slider.models import Picture
 
 
 class PictureSerializer(ModelSerializer):
+    url = SerializerMethodField()
+
     class Meta:
         model = Picture
-        exclude = ['modified']
+        exclude = ['modified', 'file', 'visible']
+
+    def get_url(self, obj):
+        return obj.file.url
